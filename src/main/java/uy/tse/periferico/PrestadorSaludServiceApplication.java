b648cbd1ab05fbc98cea8d09c1d7557a857f0ce2
Santi53197442 <santipereyra64@gmail.com>
2025-11-12 23:09:57 -0300

    prueba de edicion 2

--- src/main/java/uy/tse/periferico/PrestadorSaludServiceApplication.java
+++ src/main/java/uy/tse/periferico/PrestadorSaludServiceApplication.java
@@ -2,10 +2,13 @@ package uy.tse.periferico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import uy.tse.periferico.config.SecurityConfig;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@Import(SecurityConfig.class)
 public class PrestadorSaludServiceApplication {
 
 	public static void main(String[] args) {
